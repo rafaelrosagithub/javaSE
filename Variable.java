21d7a0560f30a16f5194575cb9113a2eef2e77ef
Rafael <rafael_unis@hotmail.com>
2022-08-17 16:35:33 -0300

    Variable concepts

---
+++ Variable.java
@@ -0,0 +1,20 @@
+/**
+ * Variable concepts
+ * @author Rafael
+ */
+
+public class Variable {
+	public static void main(String[] args) {
+		
+		String name, secondName, lastName;
+		name = "Jonh";
+		String address = "Wall Street";
+		int age = 33;
+		boolean married = true;
+		
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Address: " + address);
+		System.out.println("Married: " + married);
+	}		
+}
