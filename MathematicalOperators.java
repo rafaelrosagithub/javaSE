c9f4f8feffa233c940bb5a2bb8ca8e1998f3da3c
Rafael <rafael_unis@hotmail.com>
2022-08-25 14:47:48 -0300

    Mathematical Operators

---
+++ MathematicalOperators.java
@@ -0,0 +1,39 @@
+public class MathematicalOperators {
+	
+	//  Mathematical Operators
+	//  +		Addition
+	//  -		Subtraction
+	//  *		Multiplication
+	//  /		Division
+	//  %		Module
+	//  +		Positive
+	//	-		Negative
+	//	++		Increment +1
+	//	--		Decrement -1
+	
+	public static void main(String[] arngs) {
+		System.out.println("Mathematical Operators");
+		
+		double addition =  5 + 5;
+		double subtraction =  10 - 5;
+		double multiplication =  5 * 5;
+		double division =  10 / 2;
+		double module =  10 % 2;
+		String concatenation = "Java" + " " + "SE";
+		int x = 4;
+		int y = ++x;
+		int z = 4;
+		int w = --z;
+		
+		System.out.println("Addition: " + addition);
+		System.out.println("Subtraction: " + subtraction);
+		System.out.println("Multiplication: " + multiplication);
+		System.out.println("Division: " + division);
+		System.out.println("Module: " + module);
+		System.out.println("Concatenation: " + concatenation);
+		System.out.println("x: " + x);
+		System.out.println("y: " + y);	
+		System.out.println("z: " + z);
+		System.out.println("w: " + w);
+	}	
+}	
