8847dda51d1265b55033cb00b20338916f8eba8a
Rafael <rafael_unis@hotmail.com>
2022-08-24 22:13:46 -0300

    Operators

---
+++ Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+	public static void main(String[] args) {
+		System.out.println("Operators Class");
+		
+		// -2 unary operator
+		// 2 + 3 binary operator
+		// true ? "yes" : "no" ternary operator
+		
+		int x = 10 + 5;
+		System.out.println(x);
+		
+		String y = "10" + "5";
+		System.out.println(y);
+		
+		double z = 7 - 4 + 2 * 10;
+		System.out.println(z);
+		
+		double w = (7 - 4 + 2) * 10;
+		System.out.println(w);
+	}	
+	
+}	
