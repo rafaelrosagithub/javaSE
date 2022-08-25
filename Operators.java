10f9d21c4e4a4a6f212932ed86368754e5afedd2
Rafael <rafael_unis@hotmail.com>
2022-08-25 14:26:07 -0300

    Adjustment

--- Operators.java
+++ Operators.java
@@ -1,36 +1,37 @@
 public class Operators {
+	
+	// -2 unary operator
+	// 2 + 3 binary operator
+	// true ? "yes" : "no" ternary operator
+	
+	// Operators           Goal
+	
+	// +, -, *, /, %       Add, subtract, multiply, divide, remainder of division
+	// +				   Concatenate strings
+	// +, -				   Unary plus and minus operator
+	// <, >, <=, >=        Compare, Less Than, Greater Than, Lesser, Greater
+	// ++, --			   Pre or post increment or decrement
+	// ==, !=			   Compare if it is the same or different
+	// &&, ||			   Logical concatenation and concatenation
+	// .				   Access an object's properties
+	// []				   Index arrays
+	// ()				   Call method
+	// ?:				   Ternary, returns boolean in an expression
+	// <<, >>, >>>		   Operate numeric bit
+	// ~				   Unary complement over bits
+	// !				   Logical complement
+	// &, |, ^  		   Bit operators AND, OR and XOR
+	// ,				   Separate values
+	// =				   Assign values
+	// +=, -=, *= , /=     Assign values by adding, subtracting, multiplying and dividing
+	// <<=, >>=, >>>>=	   Byte Operator (Unary)
+	// &=, |=, ^=		   Byte operator AND, OR, XOR (unary)
+	// instanceof		   Check object type
+	// new				   Create object
+	// (type)			   Unary coercion
+	
 	public static void main(String[] args) {
 		System.out.println("Operators Class");
-		
-		// -2 unary operator
-		// 2 + 3 binary operator
-		// true ? "yes" : "no" ternary operator
-		
-		// Operators           Goal
-		
-		// +, -, *, /, %       Add, subtract, multiply, divide, remainder of division
-		// +				   Concatenate strings
-		// +, -				   Unary plus and minus operator
-		// <, >, <=, >=        Compare, Less Than, Greater Than, Lesser, Greater
-		// ++, --			   Pre or post increment or decrement
-		// ==, !=			   Compare if it is the same or different
-		// &&, ||			   Logical concatenation and concatenation
-		// .				   Access an object's properties
-		// []				   Index arrays
-		// ()				   Call method
-		// ?:				   Ternary, returns boolean in an expression
-		// <<, >>, >>>		   Operate numeric bit
-		// ~				   Unary complement over bits
-		// !				   Logical complement
-		// &, |, ^  		   Bit operators AND, OR and XOR
-		// ,				   Separate values
-		// =				   Assign values
-		// +=, -=, *= , /=     Assign values by adding, subtracting, multiplying and dividing
-		// <<=, >>=, >>>>=	   Byte Operator (Unary)
-		// &=, |=, ^=		   Byte operator AND, OR, XOR (unary)
-		// instanceof		   Check object type
-		// new				   Create object
-		// (type)			   Unary coercion
 		
 		int x = 10 + 5;
 		System.out.println(x);
