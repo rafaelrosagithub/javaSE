27af58afa471ce27a3c9c3abdbdb9cae5994f17c
Rafael <rafael_unis@hotmail.com>
2022-08-29 21:46:59 -0300

    Assignment Operators

---
+++ AssignmentOperators.java
@@ -0,0 +1,19 @@
+public class AssignmentOperators {
+	
+	// += Addition
+	// -= Subtraction
+	// *= Multiplication
+	// /= Division
+	// %= Module
+	
+	public static void main(String[] args) {
+		System.out.println("Assignement Operators");
+		
+		int x = 10;
+		System.out.println(x += 2);
+		System.out.println(x -= 2);
+		System.out.println(x *= 2);
+		System.out.println(x /= 2);
+		System.out.println(x %= 2);
+	}	
+}	
