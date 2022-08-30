eaa8c3e563b74ebcc7fc9eb1492eeebef27f0906
Rafael <rafael_unis@hotmail.com>
2022-08-29 21:37:20 -0300

    Logical Operators

---
+++ LogicalOperators.java
@@ -0,0 +1,17 @@
+public class LogicalOperators {
+	
+	// && AND 
+	// || OR
+	// ! NOT
+	
+	public static void main(String[] args) {
+		System.out.println("Logical Operators");
+		
+		int x = 7;
+		System.out.println((x >= 1) && (x <= 10));
+		System.out.println((x >= 1) && (x <= 5));
+		System.out.println((x >= 1) || (x <= 5));
+		System.out.println((x >= 1));
+		System.out.println(!(x >= 1));
+	}	
+}	
