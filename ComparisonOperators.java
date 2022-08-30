918206b94e8ed92cfd636850f6e5c984c8716fd3
Rafael <rafael_unis@hotmail.com>
2022-08-29 21:15:39 -0300

    Comparison Operators

---
+++ ComparisonOperators.java
@@ -0,0 +1,16 @@
+public class ComparisonOperators {
+	
+	// Comparison Operators
+	// ==			Equal
+	// != 			Different
+	// > 			Larger
+    // <			Smaller
+    // >=			Bigger or equal
+    // <=			Less or equal
+    // instaceof 	Compare types
+	
+	public static void main(String[] args) {
+		System.out.println("Comparison Operators");
+	}	
+	
+}
