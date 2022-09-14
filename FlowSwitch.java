d99816d1c1d08726066713b86dfeb968a632fc7e
Rafael <rafael_unis@hotmail.com>
2022-09-14 11:51:41 -0300

    Flow Switch

---
+++ FlowSwitch.java
@@ -0,0 +1,38 @@
+public class FlowSwitch {
+	
+	public static void main(String[] args) {
+		System.out.println("Flow Switch");
+		
+		
+		char sex = 'F';
+		
+		switch(sex) {
+			case 'M':
+				System.out.println("Male");
+				break;
+			case 'F':
+				System.out.println("Female");
+				break;
+			default:
+				System.out.println("Other");
+		}
+		
+		String technology = "c++";
+		
+		switch(technology) {
+			case "java":
+			case "c++":
+			case "cobol":
+				System.out.println("Programming language");
+				break;
+			case "oracle":
+			case "sqlserver":
+			case "postgresql":
+				System.out.println("Data base");
+				break;
+			default:
+				System.out.println("Unknown technology");
+		}
+	}	
+	
+}	
