a3efbb914c490b930a2f2b13af0a124a71300ef7
Rafael <rafael_unis@hotmail.com>
2022-09-28 13:34:54 -0300

    Calculating the square root

--- br/com/xyz/poo/Mathematics.java
+++ br/com/xyz/poo/Mathematics.java
@@ -4,6 +4,21 @@ public class Mathematics {
 
 	/**
 	 * 
+	 * @param number
+	 * @return The square root of a number according to pell's equation
+	 */
+	int root(int number) {
+		int root = 0, odd = 1;
+		while (number >= odd) {
+			number -= odd;
+			odd += 2;
+			++root;
+		}
+		return root;
+	}
+
+	/**
+	 * 
 	 * @param one
 	 * @param two
 	 * @return The larger of the two numbers
--- br/com/xyz/poo/MathematicsTest.java
+++ br/com/xyz/poo/MathematicsTest.java
@@ -1,22 +1,28 @@
 package br.com.xyz.poo;
 
 public class MathematicsTest {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Methods with return");
-		
+
 		Mathematics m = new Mathematics();
 		int result = m.larger(10, 20);
 		System.out.println(result);
-		
+
 		double sum = m.sum(10, 20);
 		System.out.println(sum);
-		
+
 		int odd = m.larger(2, 4);
 		int even = m.larger(3, 5);
-		
+
 		double resultSum = m.sum(odd, even);
 		System.out.println(resultSum);
+
+		int number = 300;
+		int resultRoot = m.root(number);
+		System.out.println("The square root of " + number + ": " + resultRoot);
+
+		System.out.println("Using class Math: " + Math.sqrt(number));
 	}
 
 }
