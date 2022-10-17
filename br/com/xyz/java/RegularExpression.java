0eb92f9780134785475a28721f83e2b4f4a73ea0
Rafael <rafael_unis@hotmail.com>
2022-10-17 14:52:56 -0300

    Regular expression, Metacharacters

--- br/com/xyz/java/RegularExpression.java
+++ br/com/xyz/java/RegularExpression.java
@@ -20,6 +20,43 @@ public class RegularExpression {
 		System.out.println(b);
 		b = "Java".matches("(?im)java");
 		System.out.println(b);
+		
+		// Metacharacters
+		// .  Any character	   
+		// \d Digits		   [0-9]
+		// \D It's not digit   [^0-9]
+		// \s Spaces           [ \t\n\x0B\f\r]
+		// \S It's not space   [^\s]
+		// \w Letter           [a-zA-Z_0-9]
+		// \W It's not lyrics
+		
+		b = "@".matches(".");
+		System.out.println(b);
+		b = "2".matches("\\d");
+		System.out.println(b);
+		b = "a".matches("\\d");
+		System.out.println(b);
+		b = "a".matches("\\w");
+		System.out.println(b);
+		b = "#".matches("\\w");
+		System.out.println(b);
+		b = " ".matches("\\s");
+		System.out.println(b);
+		b = "R".matches("\\s");
+		System.out.println(b);
+		b = "Pi".matches("..");
+		System.out.println(b);
+		b = "Pie".matches("...");
+		System.out.println(b);
+		b = "77".matches("\\d\\d");
+		System.out.println(b);
+		
+		String zipCode = "\\d\\d\\d\\d\\d-\\d\\d\\d";
+		b = "59987-345".matches(zipCode);
+		System.out.println(b);
+		
+		
+		
 	}
 
 }
