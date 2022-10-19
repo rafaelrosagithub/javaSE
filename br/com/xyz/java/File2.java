6d10c57d878e9a04653fbd0874d709677a88dafd
Rafael <rafael_unis@hotmail.com>
2022-10-19 14:45:55 -0300

    Writing files

---
+++ br/com/xyz/java/File2.java
@@ -0,0 +1,28 @@
+package br.com.xyz.java;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class File2 {
+
+	public static void main(String[] args) {
+		System.out.println("Working with file");
+
+		Path path = Paths.get("D:/Rafael/Projects/Courses/Java/JavaSE/xyz/files/text.txt");
+		Charset utf8 = StandardCharsets.UTF_8;
+
+		// Writing
+		try (BufferedWriter w = Files.newBufferedWriter(path, utf8)) {
+			w.write("text\n");
+			w.write("text\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
