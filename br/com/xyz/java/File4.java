6dd36c37ac7990fd6dedbfa851f8eda048f1d616
Rafael <rafael_unis@hotmail.com>
2022-10-21 17:59:06 -0300

    Created class File4, checking methods I/O

---
+++ br/com/xyz/java/File4.java
@@ -0,0 +1,35 @@
+package br.com.xyz.java;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class File4 {
+
+	// Check
+	private static void check(Path path) {
+		try {
+			System.out.println(Files.exists(path));
+			System.out.println(Files.isDirectory(path));
+			System.out.println(Files.isRegularFile(path));
+			System.out.println(Files.isReadable(path));
+			System.out.println(Files.size(path));
+			System.out.println(Files.getLastModifiedTime(path));
+			System.out.println(Files.getOwner(path));
+			System.out.println(Files.probeContentType(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Check, Delete, Create, Copy and Move");
+
+		Path path = Paths.get("D:/Rafael/Projects/Courses/Java/JavaSE/xyz/files/accounts.txt");
+
+		check(path);
+
+	}
+
+}
