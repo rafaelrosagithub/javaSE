76d3f98ce6f51314575ab49ca5b5415a70827cd7
Rafael <rafael_unis@hotmail.com>
2022-10-21 18:17:34 -0300

    Crate method, IO

--- br/com/xyz/java/File4.java
+++ br/com/xyz/java/File4.java
@@ -9,6 +9,7 @@ public class File4 {
 
 	// Check
 	private static void check(Path path) {
+		System.out.println("Check File");
 		try {
 			System.out.println(Files.exists(path));
 			System.out.println(Files.isDirectory(path));
@@ -25,6 +26,7 @@ public class File4 {
 
 	// Delete
 	private static void delete(Path path) {
+		System.out.println("Delete File");
 		try {
 			Files.delete(path);
 			Files.deleteIfExists(path);
@@ -34,13 +36,26 @@ public class File4 {
 		}
 	}
 
+	// Create
+	private static void create(Path path) {
+		System.out.println("Create File");
+		try {
+			Files.deleteIfExists(path);
+			Files.createFile(path);
+			Files.write(path, "Text1 Text2 Text3...".getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Check, Delete, Create, Copy and Move");
 
-		Path path = Paths.get("D:/Rafael/Projects/Courses/Java/JavaSE/xyz/files/accounts.txt");
+		Path path = Paths.get("D:/Rafael/Projects/Courses/Java/JavaSE/xyz/files/source.txt");
 
 //		check(path);
 //		delete(path);
+		create(path);
 
 	}
 
