75145678b28811542bb906d11bd6bd7f9bbbe5c3
Rafael <rafael_unis@hotmail.com>
2022-11-03 17:01:40 -0300

    Class Math

---
+++ br/com/xyz/java/MathTest.java
@@ -0,0 +1,20 @@
+package br.com.xyz.java;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		System.out.println("Class Math");
+
+		System.out.println(Math.E); // Euler's constant
+		System.out.println(Math.PI); // PI constant
+
+		System.out.println(Math.sin(10)); // Sine
+		System.out.println(Math.cos(10)); // Cosino
+		System.out.println(Math.tan(45)); // Tangent
+		System.out.println(Math.pow(4, 4)); // Potency
+		System.out.println(Math.sqrt(49)); // Square root
+		System.out.println(Math.cbrt(16)); // Cubic root
+		System.out.println(Math.hypot(6, 8)); // Hypotenuse
+	}
+
+}
