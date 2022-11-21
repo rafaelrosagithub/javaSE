e2c12a56691f2d26e94f3d05f11a510e779f0bf4
Rafael <rafael_unis@hotmail.com>
2022-11-20 21:38:28 -0300

    Generic

---
+++ br/com/xyz/collection/Generic.java
@@ -0,0 +1,18 @@
+package br.com.xyz.collection;
+
+public class Generic {
+
+	Object element;
+
+	public void setElement(Object element) {
+		this.element = element;
+	}
+
+	public Object getElement() {
+		return element;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Generic");
+	}
+}
