8d0f86941b09bdf5cbc265be5fb1e382f69765d0
Rafael <rafael_unis@hotmail.com>
2022-11-30 21:56:03 -0300

    Documentantion, javadoc and marks

--- src/br/com/xyz/threads/Bridge.java
+++ src/br/com/xyz/threads/Bridge.java
@@ -1,9 +1,34 @@
 package br.com.xyz.threads;
 
+/**
+ * A <code>bridge</code> represents the link between Information Producer and
+ * Consumer objects. <i>Producers</i> use Bridges to record information and
+ * share that information with Consumers who read that data from the Bridge for
+ * processing.
+ * 
+ * @author Rafael Rosa
+ * @version 1.4
+ * @since 1.0
+ * 
+ * @see BridgeNotSynchronized
+ * @see BridgeSynchronized
+ *
+ */
+
 public interface Bridge {
 
+	/**
+	 * 
+	 * @param value
+	 * @throws InterruptedException
+	 */
 	public void set(int value) throws InterruptedException;
 
-    public int get() throws InterruptedException;
+	/**
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public int get() throws InterruptedException;
 
 }
