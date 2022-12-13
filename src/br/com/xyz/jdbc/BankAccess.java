1176a22ed25d7b62ff68a6b1a38063ab668fe29a
Rafael <rafael_unis@hotmail.com>
2022-12-12 22:15:52 -0300

    Bank Access with JDBC

---
+++ src/br/com/xyz/jdbc/BankAccess.java
@@ -0,0 +1,26 @@
+package br.com.xyz.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BankAccess {
+
+	public static void main(String[] args) {
+		System.out.println("Bank Access with JDBC");
+
+		String sql = "select code, name, sex, email from person";
+		String url = "jdbc:postgresql:localhost:5432:teste";
+		try (Connection con = DriverManager.getConnection(url, "postgres", "tcc2");
+				PreparedStatement stm = con.prepareStatement(url);
+				ResultSet rs = stm.executeQuery()) {
+			while (rs.next()) {
+				System.out.println(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
